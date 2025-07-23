e1e1a5556d0ed6d91b67b32d5090c806724c9193
rezaali2290 <rezaali20ab@gmail.com>
2025-07-23 23:43:48 +0545

    product logic

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -34,8 +34,47 @@ import {
   UPDATE_PRODUCT_SUCCESS,
 } from "../constants/ProductConstants";
 
+export const productreducer = (state = { products: [] }, action) => {
+  switch (action.type) {
+    case ALL_PRODUCT_REQUEST:
+    case ADMIN_PRODUCT_REQUEST:
+      return {
+        loading: true,
+        products: [],
+      };
 
+    case ALL_PRODUCT_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload.products,
+        productsCount: action.payload.productsCount,
+        resultPerPage: action.payload.resultPerPage,
+        filteredProductsCount: action.payload.filteredProductsCount,
+      };
+    case ADMIN_PRODUCT_SUCCESS:
+      return {
+        loading: false,
+        products: action.payload,
+      };
+    case ALL_PRODUCT_FAIL:
+    case ADMIN_PRODUCT_FAIL:
+      return {
+        loading: false,
 
+        error: action.payload,
+      };
+
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+
+        error: null,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export const productDetailsReducer = (state = { product: {} }, action) => {
   switch (action.type) {
