f80aab1917271f9f35f70f4e42a7c8c26aa7fa47
rezaali2290 <rezaali20ab@gmail.com>
2025-07-24 01:49:30 +0545

    AllUsers admin controller

---
+++ src/component/Admin/AllUsers.jsx
@@ -0,0 +1,99 @@
+import React, { Fragment, useEffect } from "react";
+import { DataGrid } from "@material-ui/data-grid";
+import "./CreateProduct.css";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import MetaData from "../../more/MetaData";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SideBar from "./Sidebar";
+import { getAllUsers, clearErrors, deleteUser } from "../../actions/UserActions";
+import { DELETE_USER_RESET } from "../../constants/UserConstants";
+import { ToastContainer, toast } from "react-toastify";
+
+const AllUsers = ({ history }) => {
+  const dispatch = useDispatch();
+
+  const { error, users } = useSelector((state) => state.allUsers);
+
+  const {
+    error: deleteError,
+    isDeleted,
+    message,
+  } = useSelector((state) => state.profile);
+
+  const deleteUserHandler = (id) => {
+    dispatch(deleteUser(id));
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      dispatch(clearErrors());
+    }
+
+    if (deleteError) {
+      toast.error(deleteError);
+      dispatch(clearErrors());
+    }
+
+    if (isDeleted) {
+      toast.success(message);
+      history.push("/admin/users");
+      dispatch({ type: DELETE_USER_RESET });
+    }
+
+    dispatch(getAllUsers());
+  }, [dispatch, alert, error, deleteError, history, isDeleted, message]);
+
+  const columns = [
+    { field: "id", headerName: "User ID", minWidth: 180, flex: 0.8 },
+
+    {
+      field: "email",
+      headerName: "Email",
+      minWidth: 200,
+      flex: 1,
+    },
+    {
+      field: "name",
+      headerName: "Name",
+      minWidth: 150,
+      flex: 0.5,
+    },
+
+    {
+      field: "role",
+      headerName: "Role",
+      type: "number",
+      minWidth: 150,
+      flex: 0.3,
+      cellClassName: (params) => {
+        return params.getValue(params.id, "role") === "admin"
+          ? "greenColor"
+          : "redColor";
+      },
+    },
+
+    
+  ];
+
+  const rows = [];
+
+  users &&
+    users.forEach((item) => {
+      rows.push({
+        id: item._id,
+        role: item.role,
+        email: item.email,
+        name: item.name,
+      });
+    });
+
+  return (
+    
+  );
+};
+
+export default AllUsers;
